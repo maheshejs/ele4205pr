1ce9706dab0e5f7d718b5f6a649cbf66ddb8a553
Joseph Maheshe <maheshejs@gmail.com>
2021-10-25 19:21:27 -0400

    Livrable2 : fixed indentation

--- src/boneCVtiming.cpp
+++ src/boneCVtiming.cpp
@@ -19,62 +19,62 @@ using namespace std;
 using namespace cv;
 
 struct timing {
-	int resX;
-	int resY;
-	double fps;
+    int resX;
+    int resY;
+    double fps;
 };
 
 int main()
 {
-	struct timing usbCameraTimings[NUMBER_OF_RESOLUTIONS] = {
+    struct timing usbCameraTimings[NUMBER_OF_RESOLUTIONS] = {
         {176, 144, 30.0}, {160, 120, 30.0}, {320, 176, 30.0}, {320, 240, 30.0},
         {352, 288, 30.0}, {432, 240, 30.0}, {800, 600, 30.0}, {864, 480, 30.0},
         {960, 544, 30.0}, {960, 720, 30.0}, {1184, 656, 30.0}, {1280, 720, 30.0},
-		{1280, 960, 30.0}
+        {1280, 960, 30.0}
     };
 
     for (int i = 0; i < NUMBER_OF_RESOLUTIONS; i++)
     {
-		cout << "Calculating framerate for " << usbCameraTimings[i].resX << "x" << usbCameraTimings[i].resY << endl;
-		VideoCapture capture(0);
-		capture.set(CV_CAP_PROP_FRAME_WIDTH, usbCameraTimings[i].resX);
-		capture.set(CV_CAP_PROP_FRAME_HEIGHT, usbCameraTimings[i].resY);
-		if(!capture.isOpened()){
-			cout << "Failed to connect to the camera." << endl;
-		}
-		Mat frame, edges;
+        cout << "Calculating framerate for " << usbCameraTimings[i].resX << "x" << usbCameraTimings[i].resY << endl;
+        VideoCapture capture(0);
+        capture.set(CV_CAP_PROP_FRAME_WIDTH, usbCameraTimings[i].resX);
+        capture.set(CV_CAP_PROP_FRAME_HEIGHT, usbCameraTimings[i].resY);
+        if(!capture.isOpened()){
+            cout << "Failed to connect to the camera." << endl;
+        }
+        Mat frame, edges;
 
-		// We read 2 frames to make sure new resolution is well initialized before measuring time
-		capture >> frame;
-		capture >> frame;
+        // We read 2 frames to make sure new resolution is well initialized before measuring time
+        capture >> frame;
+        capture >> frame;
 
-		struct timespec start, end;
-		clock_gettime( CLOCK_REALTIME, &start );
+        struct timespec start, end;
+        clock_gettime( CLOCK_REALTIME, &start );
 
-		int frames = 10;
-		for(int j = 0; j < frames; j++){
-			capture >> frame;
-			if(frame.empty()){
-			cout << "Failed to capture an image" << endl;
-			return -1;
-			}
-			cvtColor(frame, edges, CV_BGR2GRAY);
-			Canny(edges, edges, 0, 30, 3);
-		}
+        int frames = 10;
+        for(int j = 0; j < frames; j++){
+            capture >> frame;
+            if(frame.empty()){
+            cout << "Failed to capture an image" << endl;
+            return -1;
+            }
+            cvtColor(frame, edges, CV_BGR2GRAY);
+            Canny(edges, edges, 0, 30, 3);
+        }
 
-		clock_gettime( CLOCK_REALTIME, &end );
-		double difference = (end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/1000000000.0d;
-		usbCameraTimings[i].fps = frames/difference;
+        clock_gettime( CLOCK_REALTIME, &end );
+        double difference = (end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/1000000000.0d;
+        usbCameraTimings[i].fps = frames/difference;
 
-		imwrite("edges.png", edges);
-		imwrite("capture.png", frame);
+        imwrite("edges.png", edges);
+        imwrite("capture.png", frame);
     }
 
-	cout << "Framerate for every resolution :" << endl;
-	for (int i = 0; i < NUMBER_OF_RESOLUTIONS; i++)
-	{
-		cout << usbCameraTimings[i].resX << "x" << usbCameraTimings[i].resY << " : " << usbCameraTimings[i].fps << " frames/second" << endl;
-	}
+    cout << "Framerate for every resolution :" << endl;
+    for (int i = 0; i < NUMBER_OF_RESOLUTIONS; i++)
+    {
+        cout << usbCameraTimings[i].resX << "x" << usbCameraTimings[i].resY << " : " << usbCameraTimings[i].fps << " frames/second" << endl;
+    }
 
     return 0;
 }
--- src/livrable1.cpp
+++ src/livrable1.cpp
@@ -7,55 +7,55 @@ using namespace std;
 using namespace cv;
 
 struct timing {
-	int resX;
-	int resY;
-	double fps;
+    int resX;
+    int resY;
+    double fps;
 };
 
 struct timing usbCameraTimings[NUMBER_OF_RESOLUTIONS] = {
         {176, 144, 30.0}, {160, 120, 30.0}, {320, 176, 30.0}, {320, 240, 30.0},
         {352, 288, 30.0}, {432, 240, 30.0}, {800, 600, 30.0}, {864, 480, 30.0},
         {960, 544, 30.0}, {960, 720, 30.0}, {1184, 656, 30.0}, {1280, 720, 30.0},
-		{1280, 960, 30.0}
+        {1280, 960, 30.0}
 };
 
 int main()
 {
-	int resolutionChoice = 0;
-	cout << "List of supported resolutions : " << endl;
-	for (int i = 0; i < NUMBER_OF_RESOLUTIONS; i++)
-	{
-		cout << i+1 << ") " << usbCameraTimings[i].resX << "x" << usbCameraTimings[i].resY << endl;
-	}
-	cout << "Please select resolution : ";
-	cin >> resolutionChoice;
+    int resolutionChoice = 0;
+    cout << "List of supported resolutions : " << endl;
+    for (int i = 0; i < NUMBER_OF_RESOLUTIONS; i++)
+    {
+        cout << i+1 << ") " << usbCameraTimings[i].resX << "x" << usbCameraTimings[i].resY << endl;
+    }
+    cout << "Please select resolution : ";
+    cin >> resolutionChoice;
 
-	double fps = usbCameraTimings[resolutionChoice - 1].fps;
-	int resX = usbCameraTimings[resolutionChoice - 1].resX;
-	int resY = usbCameraTimings[resolutionChoice - 1].resY;
-	double length = 5.0;
-	VideoWriter out("capture-liv1.avi", CV_FOURCC('M', 'J', 'P', 'G'), fps, Size(resX, resY));
-	VideoCapture capture(0);
-	capture.set(CV_CAP_PROP_FRAME_WIDTH, resX);
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT, resY);
-	if(!capture.isOpened()){
-		cout << "Failed to connect to the camera." << endl;
-	}
+    double fps = usbCameraTimings[resolutionChoice - 1].fps;
+    int resX = usbCameraTimings[resolutionChoice - 1].resX;
+    int resY = usbCameraTimings[resolutionChoice - 1].resY;
+    double length = 5.0;
+    VideoWriter out("capture-liv1.avi", CV_FOURCC('M', 'J', 'P', 'G'), fps, Size(resX, resY));
+    VideoCapture capture(0);
+    capture.set(CV_CAP_PROP_FRAME_WIDTH, resX);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT, resY);
+    if(!capture.isOpened()){
+        cout << "Failed to connect to the camera." << endl;
+    }
 
-	Mat frame;
-	int frameCount = static_cast<int>(fps*length);
+    Mat frame;
+    int frameCount = static_cast<int>(fps*length);
 
-	for(int i = 0; i < frameCount; i++){
-		capture >> frame;
-		if (frame.empty())
-		{
-			cout << "Failed to capture an image" << endl;
-			return -1;
-		}
-		else
-		{
-			out.write(frame);
-		}
-	}
-	return 0;
+    for(int i = 0; i < frameCount; i++){
+        capture >> frame;
+        if (frame.empty())
+        {
+            cout << "Failed to capture an image" << endl;
+            return -1;
+        }
+        else
+        {
+            out.write(frame);
+        }
+    }
+    return 0;
 }
